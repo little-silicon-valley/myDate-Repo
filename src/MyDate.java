3c7f62df8b20226f4c7660dabf0086bf21130504
Mahsa <mahsa.alikhany@gmail.com>
2021-09-20 20:18:37 +0430

    add getter and setter to MyDate java class.

--- src/MyDate.java
+++ src/MyDate.java
@@ -17,4 +17,28 @@ public class MyDate {
         this.month = month;
         this.day = day;
     }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
 }
