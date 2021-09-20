50e5e65bfd843ed3dcb0886b754d84d99c234b85
Mahdi-Jhr <jahanshir.sm92@gmail.com>
2021-09-20 20:16:33 +0430

    add getter method

--- src/MyDate.java
+++ src/MyDate.java
@@ -12,4 +12,16 @@ public class MyDate {
         this.month = month;
         this.day = day;
     }
+    public int getYear() {
+        return year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
 }
