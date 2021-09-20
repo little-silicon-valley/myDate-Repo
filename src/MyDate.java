ba39c5f94b329bc32fe80a801aa24facc7026725
Mahdi-Jhr <jahanshir.sm92@gmail.com>
2021-09-20 20:18:10 +0430

    add isValidDate method

--- src/MyDate.java
+++ src/MyDate.java
@@ -23,5 +23,19 @@ public class MyDate {
     public int getDay() {
         return day;
     }
+    public boolean isValidDate(int year, int month, int day) {
+        if (year > 0 && year <= 9999 && month > 0 && month <= 12) {
+            if (month > 0 && month < 7 && day > 0 && day <=31) {
+                return true;
+            } else if (month > 7 && month < 12 && day > 0 && day <= 30) {
+                return true;
+            } else if (month == 12 && day > 0 && day <= 29) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
 
 }
