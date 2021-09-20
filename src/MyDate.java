46d25e6f216b1768ed9c60a6b1b27f73f5a2a35b
Mahsa <mahsa.alikhany@gmail.com>
2021-09-20 20:16:08 +0430

    add enum Month to MyDate java class.

--- src/MyDate.java
+++ src/MyDate.java
@@ -2,5 +2,9 @@
  * @author Mahsa Alikhani m-58
  */
 public class MyDate {
-    
+
+    enum Month {
+        FARVARDIN, ORDIBEHESHT, KHORDAD, TIR, MORDAD, SHAHRIVAR,
+        MEHR, ABAN, AZAR, DEY, BAHMAN, ESFAND;
+    }
 }
