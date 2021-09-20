5ed956cdd454f203980cdea7f4a74278a4729def
Mahsa <mahsa.alikhany@gmail.com>
2021-09-20 20:17:39 +0430

    add constructor to MyDate java class.

--- src/MyDate.java
+++ src/MyDate.java
@@ -7,4 +7,14 @@ public class MyDate {
         FARVARDIN, ORDIBEHESHT, KHORDAD, TIR, MORDAD, SHAHRIVAR,
         MEHR, ABAN, AZAR, DEY, BAHMAN, ESFAND;
     }
+
+    private int year;
+    private int month;
+    private int day;
+
+    public MyDate(int year, int month, int day) {
+        this.year = year;
+        this.month = month;
+        this.day = day;
+    }
 }
